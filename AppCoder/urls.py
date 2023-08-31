5c784a4e5a34bdb8bffde2123105af11f47865c5
Franco Magrini <franco_magrini@hotmail.com>
2023-08-31 15:26:34 -0300

    final

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     path('usuarios_create/', usuarios_create, name="usuarios_create"),
     path('cancha_padel_create/', cancha_padel_create, name="cancha_padel_create"),
     path('cancha_futbol_create/', cancha_futbol_create, name="cancha_futbol_create"),
-  #  path('busquedacomision/', busquedacomision, name="busquedacomision"),
-  #  path('buscar/', buscar, name="buscar"),
     path('verificar_disponibilidad/', verificar_disponibilidad, name='verificar_disponibilidad'),
     
 ]
